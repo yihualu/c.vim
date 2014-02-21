9d2061f47851e82da623c863eba9d95cfc391e1e
Edward Lu <edward@Edwards-MacBook-Pro.local>
2014-02-21 14:59:10 +0800

    update to 6.1

--- c-support/codesnippets/calloc_int_matrix.c
+++ c-support/codesnippets/calloc_int_matrix.c
@@ -1,19 +1,21 @@
+
 /*
  * ===  FUNCTION  ======================================================================
  *         Name:  calloc_int_matrix
  *  Description:  Allocate a dynamic int-matrix of size rows*columns; return a pointer.
  * =====================================================================================
  */
-int** calloc_int_matrix(int rows, int columns)
+int**
+calloc_int_matrix ( int rows, int columns )
 {
   int   i;
   int **m;
-  m = calloc(rows, sizeof(int*));                   /* allocate pointer array     */
-  assert(m != NULL);                                /* abort if allocation failed */
-  *m = calloc(rows*columns, sizeof(int));           /* allocate data array        */
-  assert(*m != NULL);                               /* abort if allocation failed */
-  for (i = 1; i < rows; i += 1)                     /* set pointers               */
-    m[i] = m[i-1] + columns;
+  m     = calloc ( rows, sizeof(int*) );        /* allocate pointer array     */
+  assert( m != NULL );                          /* abort if allocation failed */
+  m[0]  = calloc ( rows*columns, sizeof(int) ); /* allocate data array        */
+  assert(*m != NULL );                          /* abort if allocation failed */
+  for ( i=1; i<rows; i+=1 )                     /* set pointers               */
+    m[i]  = m[i-1] + columns;
   return m;
 }  /* ----------  end of function calloc_int_matrix  ---------- */
 
@@ -23,10 +25,11 @@ int** calloc_int_matrix(int rows, int columns)
  *  Description:  Free a dynamic int-matrix.
  * =====================================================================================
  */
-void free_int_matrix(int **m)
+int**
+free_int_matrix ( int **m )
 {
-  free(*m);                                    /* free data array            */
-  free(m);                                     /* free pointer array         */
-  return;
+  free(*m);                                     /* free data array            */
+  free( m);                                     /* free pointer array         */
+  return NULL;
 }  /* ----------  end of function free_int_matrix  ---------- */
 
